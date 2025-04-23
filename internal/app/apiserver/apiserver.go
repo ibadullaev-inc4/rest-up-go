33c9c21ad02d3fcead36bac02967bab11da5b16c
Nariman Ibadullaev <n.ibadullaev@inc4.net>
2025-04-23 17:54:49 +0400

    fix: add db postgres

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -3,6 +3,7 @@ package apiserver
 import (
 	"io"
 	"net/http"
+	"rest-up-go/internal/app/store"
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
@@ -13,6 +14,7 @@ type APIServer struct {
 	config *Config
 	logger *logrus.Logger
 	router *mux.Router
+	store  *store.Store
 }
 
 // New creates a new instance of APIServer.
@@ -30,6 +32,11 @@ func (s *APIServer) Start() error {
 		return err
 	}
 	s.configureRouter()
+
+	if err := s.configureStrore(); err != nil {
+		return err
+	}
+
 	s.logger.Infof("Starting server on %s", s.config.BindAddr)
 	s.logger.Info("Router configured")
 	return http.ListenAndServe(s.config.BindAddr, s.router)
@@ -46,6 +53,15 @@ func (s *APIServer) configureLogger() error {
 
 func (s *APIServer) configureRouter() {
 	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) configureStrore() error {
+	st := store.New(&s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+	s.store = st
+	return nil
 }
 
 func (s *APIServer) handleHello() http.HandlerFunc {
