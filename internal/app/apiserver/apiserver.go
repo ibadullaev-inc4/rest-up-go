c2c20499a576701e4f652fb825df864ee7224eba
Nariman Ibadullaev <n.ibadullaev@inc4.net>
2025-04-23 14:28:44 +0400

    fix: add logger

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,18 +1,35 @@
 package apiserver
 
+import "github.com/sirupsen/logrus"
+
 // APIServer is a struct that represents the API server.
 type APIServer struct {
 	config *Config
+	logger *logrus.Logger
 }
 
 // New creates a new instance of APIServer.
 func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
+		logger: logrus.New(),
 	}
 }
 
 // Start the API server.
 func (s *APIServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+	s.logger.Infof("Starting server on %s", s.config.BindAddr)
+	return nil
+}
+
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+	s.logger.SetLevel(level)
 	return nil
 }
