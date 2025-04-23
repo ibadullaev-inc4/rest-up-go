eb5aee4b5b12552c02a05569078cde5feca8ea4d
Nariman Ibadullaev <n.ibadullaev@inc4.net>
2025-04-23 15:10:31 +0400

    fix: add handler layer

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.24.1
 
 require (
 	github.com/BurntSushi/toml v1.5.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,11 +1,18 @@
 package apiserver
 
-import "github.com/sirupsen/logrus"
+import (
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
 
 // APIServer is a struct that represents the API server.
 type APIServer struct {
 	config *Config
 	logger *logrus.Logger
+	router *mux.Router
 }
 
 // New creates a new instance of APIServer.
@@ -13,6 +20,7 @@ func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
 		logger: logrus.New(),
+		router: mux.NewRouter(),
 	}
 }
 
@@ -21,8 +29,10 @@ func (s *APIServer) Start() error {
 	if err := s.configureLogger(); err != nil {
 		return err
 	}
+	s.configureRouter()
 	s.logger.Infof("Starting server on %s", s.config.BindAddr)
-	return nil
+	s.logger.Info("Router configured")
+	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
 
 func (s *APIServer) configureLogger() error {
@@ -32,4 +42,14 @@ func (s *APIServer) configureLogger() error {
 	}
 	s.logger.SetLevel(level)
 	return nil
+}
+
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello, world!")
+	}
 }
